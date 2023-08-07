90bd1336b721330f8712c9ab8b7c7450348f1e34
nabilaba <nabilazizbimaanggita@gmail.com>
2023-08-07 22:53:47 +0700

    added conf for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,19 @@ module.exports = {
       __key: "pages",
     },
     {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/*": ["Strict-Transport-Security: max-age=63072000"],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+    {
       resolve: "@chakra-ui/gatsby-plugin",
       options: {
         /**
