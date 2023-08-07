64062c7ac3d543c5870425bc40ef3f08dcb082a7
nabilaba <nabilazizbimaanggita@gmail.com>
2023-08-07 23:50:51 +0700

    try fix lang

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,8 +1,14 @@
-import React from "react"
-import { useSiteMetadata } from "../hooks/use-site-metadata"
+import React from "react";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
 const SEO = ({ title, description, pathname, children }) => {
-  const { title: defaultTitle, description: defaultDescription, image, siteUrl, twitterUsername } = useSiteMetadata()
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    image,
+    siteUrl,
+    twitterUsername,
+  } = useSiteMetadata();
 
   const seo = {
     title: title || defaultTitle,
@@ -10,13 +16,18 @@ const SEO = ({ title, description, pathname, children }) => {
     image: `${siteUrl}${image}`,
     url: `${siteUrl}${pathname || ``}`,
     twitterUsername,
-  }
+  };
 
   return (
     <>
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
@@ -25,7 +36,7 @@ const SEO = ({ title, description, pathname, children }) => {
       <meta name="twitter:creator" content={seo.twitterUsername} />
       {children}
     </>
-  )
-}
+  );
+};
 
 export default SEO;
