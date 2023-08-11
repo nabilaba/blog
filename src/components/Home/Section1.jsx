d7f1fd3783a4c8715f68dcc3e8e5880a1f93d6ee
nabilaba <nabilazizbimaanggita@gmail.com>
2023-08-11 11:00:04 +0700

    jos

--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -1,13 +1,93 @@
 import * as React from "react";
 import { Link } from "gatsby-link";
-import { Box, Grid, HStack, Text } from "@chakra-ui/layout";
-import { Image } from "@chakra-ui/image";
-import { getImage } from "gatsby-plugin-image";
+import { Box, Grid, HStack, Stack, Text } from "@chakra-ui/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const Section1 = () => {
-  const img =
-    "https://asset.kompas.com/crops/z_mS8xWgJw0nYWgXPh6zqGiYXto=/0x0:5472x3648/750x500/data/photo/2019/10/25/5db2e12290693.jpg";
-  const loadImg = getImage(img) || img;
+const Section1 = ({ posts }) => {
+  if (!posts) return null;
+  if (!Array.isArray(posts)) return null;
+
+  const postsList = posts?.map((post, i) => {
+    return (
+      <Box
+        as={Link}
+        to={`/blog/${post.slug}`}
+        key={i}
+        pos="relative"
+        w="100%"
+        h="250px"
+        rounded="xl"
+        overflow="hidden"
+        _hover={{
+          ".img": {
+            transform: "scale(1.1)",
+          },
+        }}
+      >
+        <GatsbyImage
+          image={getImage(post.heroImage)}
+          alt={post.title}
+          className="img"
+          style={{
+            transition: "transform 0.5s ease",
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+        <Box
+          pos="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgGradient="linear(to-t, blackAlpha.900 20%, transparent)"
+        />
+        <Box
+          pos="absolute"
+          bottom="0"
+          left="0"
+          right="0"
+          p="2"
+          fontSize="md"
+          color="white"
+        >
+          <Text
+            fontSize="90%"
+            style={{
+              display: "-webkit-box",
+              WebkitLineClamp: "2",
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {post.title}
+          </Text>
+          <Stack>
+            <Text fontSize="80%" color="gray.400">
+              {post.publishDate}
+            </Text>
+          </Stack>
+          <HStack spacing="1" mt="1">
+            {post.tags?.map((tag, i) => (
+              <Text
+                key={i}
+                fontSize="80%"
+                bg="red.600"
+                px="1"
+                rounded="sm"
+                w="fit-content"
+              >
+                {tag}
+              </Text>
+            ))}
+          </HStack>
+        </Box>
+      </Box>
+    );
+  });
+
   return (
     <>
       <Grid
@@ -18,78 +98,7 @@ const Section1 = () => {
         }}
         gap={2}
       >
-        {Object.keys(Array.from(Array(4))).map((_, i) => (
-          <Box
-            as={Link}
-            to={`/article${i}`}
-            key={i}
-            pos="relative"
-            w="100%"
-            h="calc(100vh / 4)"
-            rounded="xl"
-            overflow="hidden"
-            _hover={{
-              ".img": {
-                transform: "scale(1.1)",
-              },
-            }}
-          >
-            <Image
-              className="img"
-              src={loadImg}
-              alt="..."
-              objectFit="cover"
-              w="100%"
-              h="100%"
-              transition="all 0.3s ease-in-out"
-            />
-            <Box
-              pos="absolute"
-              top="0"
-              left="0"
-              right="0"
-              bottom="0"
-              bgGradient="linear(to-t, blackAlpha.900 20%, transparent)"
-            />
-            <Box
-              pos="absolute"
-              bottom="0"
-              left="0"
-              right="0"
-              p="2"
-              fontSize="md"
-              color="white"
-            >
-              <Text
-                fontSize="90%"
-                style={{
-                  display: "-webkit-box",
-                  WebkitLineClamp: "2",
-                  WebkitBoxOrient: "vertical",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                }}
-              >
-                Partai geram dengan pemerintah yang tidak konsisten dalam
-                menangani pandemi covid-19 di Indonesia
-              </Text>
-              <HStack spacing="1" mt="1">
-                <Text
-                  fontSize="80%"
-                  bg="red.600"
-                  px="1"
-                  rounded="sm"
-                  w="fit-content"
-                >
-                  Berita
-                </Text>
-                <Text fontSize="80%" color="gray.400">
-                  2 menit lalu
-                </Text>
-              </HStack>
-            </Box>
-          </Box>
-        ))}
+        {postsList}
       </Grid>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,17 @@ import SEOComponent from "../components/SEOComponent";
 import Layout from "../components/Layout";
 import Section1 from "../components/Home/Section1";
 import Headline from "../components/Home/Headline";
+import { graphql } from "gatsby";
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  const posts = data.allContentfulBlogPost.nodes;
   return (
     <>
       <Layout>
         <Headline />
-        <Section1 />
+        <Section1 posts={posts} />
       </Layout>
     </>
   );
@@ -20,3 +24,22 @@ export default IndexPage;
 export const Head = () => {
   return <SEOComponent />;
 };
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    allContentfulBlogPost(sort: { publishDate: DESC }) {
+      nodes {
+        description {
+          raw
+        }
+        title
+        slug
+        publishDate(locale: "ID", formatString: "DD MMMM YYYY")
+        tags
+        heroImage {
+          gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+        }
+      }
+    }
+  }
+`;
